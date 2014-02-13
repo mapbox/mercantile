12fe57f5b836a28720e5b59da4be7d488982e842
Sean Gillies <sean@mapbox.com>
2014-02-12 22:14:10 -0700

    Attempt at using namedtuples to satisfy all.

--- mercantile/__init__.py
+++ mercantile/__init__.py
@@ -2,8 +2,11 @@
 
 # http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
 
+from collections import namedtuple
 import math
 
+LngLat = namedtuple('LngLat', ['lng', 'lat'])
+LngLatBbox = namedtuple('LngLatBbox', ['west', 'south', 'east', 'north'])
 
 def ul(xtile, ytile, zoom):
     """Returns the upper left (lon, lat) of a tile"""
@@ -11,17 +14,17 @@ def ul(xtile, ytile, zoom):
     lon_deg = xtile / n * 360.0 - 180.0
     lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
     lat_deg = math.degrees(lat_rad)
-    return (lon_deg, lat_deg)
+    return LngLat(lon_deg, lat_deg)
 
 def bbox(xtile, ytile, zoom):
     """Returns the (lon, lat) bounding box of a tile"""
     a = ul(xtile, ytile, zoom)
     b = ul(xtile+1, ytile+1, zoom)
-    return (a[0], b[1], b[0], a[1])
+    return LngLatBbox(a[0], b[1], b[0], a[1])
 
-def xy(lon, lat):
+def xy(lng, lat):
     """Returns the Spherical Mercator (x, y) in meters"""
-    x = 6378137.0 * math.radians(lon)
+    x = 6378137.0 * math.radians(lng)
     y = 6378137.0 * math.log(
         math.tan((math.pi*0.25) + (0.5 * math.radians(lat))) )
     return x, y
--- mercantile/tests.py
+++ mercantile/tests.py
@@ -2,19 +2,29 @@ import mercantile
 
 def test_ul():
     expected = (-9.140625, 53.33087298301705)
-    lonlat = mercantile.ul(486, 332, 10)
-    for a, b in zip(expected, lonlat):
+    lnglat = mercantile.ul(486, 332, 10)
+    for a, b in zip(expected, lnglat):
         assert round(a-b, 7) == 0
+    assert lnglat[0] == lnglat.lng
+    assert lnglat[1] == lnglat.lat
 
 def test_bbox():
     expected = (-9.140625, 53.12040528310657, -8.7890625, 53.33087298301705)
     bbox = mercantile.bbox(486, 332, 10)
     for a, b in zip(expected, bbox):
         assert round(a-b, 7) == 0
+    assert bbox.west == bbox[0]
+    assert bbox.south == bbox[1]
+    assert bbox.east == bbox[2]
+    assert bbox.north == bbox[3]
 
 def test_xy():
     ul = mercantile.ul(486, 332, 10)
     xy = mercantile.xy(*ul)
     expected = (-1017529.7205322663, 7044436.526761846)
+    for a, b in zip(expected, xy):
+        assert round(a-b, 7) == 0
+    xy = mercantile.xy(0.0, 0.0)
+    expected = (0.0, 0.0)
     for a, b in zip(expected, xy):
         assert round(a-b, 7) == 0
