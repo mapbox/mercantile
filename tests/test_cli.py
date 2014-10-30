36d17d0a4ebd52bfdc8967c596f74343ac430e5e
Sean Gillies <sean@mapbox.com>
2014-10-30 13:00:20 -0600

    Remove subprocess from CLI tests.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,138 +1,147 @@
 import json
-import subprocess
+
+from click.testing import CliRunner
 
 import mercantile
+from mercantile.scripts import cli
 
 
 def test_cli_shapes():
-    result = subprocess.check_output(
-        'echo "[106, 193, 9]" | mercantile shapes - --precision 6',
-        shell=True)
-    assert result.decode('utf-8').strip() == '{"bbox": [-105.46875, 39.909736, -104.765625, 40.446947], "geometry": {"coordinates": [[[-105.46875, 39.909736], [-105.46875, 40.446947], [-104.765625, 40.446947], [-104.765625, 39.909736], [-105.46875, 39.909736]]], "type": "Polygon"}, "id": "(106, 193, 9)", "properties": {"title": "XYZ tile (106, 193, 9)"}, "type": "Feature"}'
-
-
-def test_cli_shapes_implicit_stdin():
-    result = subprocess.check_output(
-        'echo "[106, 193, 9]" | mercantile shapes --precision 6',
-        shell=True)
-    assert result.decode('utf-8').strip() == '{"bbox": [-105.46875, 39.909736, -104.765625, 40.446947], "geometry": {"coordinates": [[[-105.46875, 39.909736], [-105.46875, 40.446947], [-104.765625, 40.446947], [-104.765625, 39.909736], [-105.46875, 39.909736]]], "type": "Polygon"}, "id": "(106, 193, 9)", "properties": {"title": "XYZ tile (106, 193, 9)"}, "type": "Feature"}'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['shapes', '--precision', '6'], "[106, 193, 9]")
+    assert result.exit_code == 0
+    assert result.output == '{"bbox": [-105.46875, 39.909736, -104.765625, 40.446947], "geometry": {"coordinates": [[[-105.46875, 39.909736], [-105.46875, 40.446947], [-104.765625, 40.446947], [-104.765625, 39.909736], [-105.46875, 39.909736]]], "type": "Polygon"}, "id": "(106, 193, 9)", "properties": {"title": "XYZ tile (106, 193, 9)"}, "type": "Feature"}\n'
 
 
 def test_cli_shapes_arg():
-    result = subprocess.check_output(
-        'mercantile shapes "[106, 193, 9]" --precision 6',
-        shell=True)
-    assert result.decode('utf-8').strip() == '{"bbox": [-105.46875, 39.909736, -104.765625, 40.446947], "geometry": {"coordinates": [[[-105.46875, 39.909736], [-105.46875, 40.446947], [-104.765625, 40.446947], [-104.765625, 39.909736], [-105.46875, 39.909736]]], "type": "Polygon"}, "id": "(106, 193, 9)", "properties": {"title": "XYZ tile (106, 193, 9)"}, "type": "Feature"}'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['shapes', "[106, 193, 9]", '--precision', '6'])
+    assert result.exit_code == 0
+    assert result.output == '{"bbox": [-105.46875, 39.909736, -104.765625, 40.446947], "geometry": {"coordinates": [[[-105.46875, 39.909736], [-105.46875, 40.446947], [-104.765625, 40.446947], [-104.765625, 39.909736], [-105.46875, 39.909736]]], "type": "Polygon"}, "id": "(106, 193, 9)", "properties": {"title": "XYZ tile (106, 193, 9)"}, "type": "Feature"}\n'
+
 
 def test_cli_shapes_buffer():
-    result = subprocess.check_output(
-        'mercantile shapes "[106, 193, 9]" --buffer 1.0 --precision 6',
-        shell=True)
-    assert result.decode('utf-8').strip() == '{"bbox": [-106.46875, 38.909736, -103.765625, 41.446947], "geometry": {"coordinates": [[[-106.46875, 38.909736], [-106.46875, 41.446947], [-103.765625, 41.446947], [-103.765625, 38.909736], [-106.46875, 38.909736]]], "type": "Polygon"}, "id": "(106, 193, 9)", "properties": {"title": "XYZ tile (106, 193, 9)"}, "type": "Feature"}'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['shapes', '[106, 193, 9]', '--buffer', '1.0', '--precision', '6'])
+    assert result.exit_code == 0
+    assert result.output == '{"bbox": [-106.46875, 38.909736, -103.765625, 41.446947], "geometry": {"coordinates": [[[-106.46875, 38.909736], [-106.46875, 41.446947], [-103.765625, 41.446947], [-103.765625, 38.909736], [-106.46875, 38.909736]]], "type": "Polygon"}, "id": "(106, 193, 9)", "properties": {"title": "XYZ tile (106, 193, 9)"}, "type": "Feature"}\n'
+
 
 def test_cli_shapes_extents():
-    result = subprocess.check_output(
-        'mercantile shapes "[106, 193, 9]" --extents --mercator --precision 3',
-        shell=True)
-    assert result.decode('utf-8').strip() == '-11740727.545 4852834.052 -11662456.028 4931105.569'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['shapes', '[106, 193, 9]', '--extents', '--mercator', '--precision', '3'])
+    assert result.exit_code == 0
+    assert result.output == '-11740727.545 4852834.052 -11662456.028 4931105.569\n'
 
 
 def test_cli_shapes_props():
-    result = subprocess.check_output(
-            """mercantile shapes '{"tile": [106, 193, 9], "properties": {"title": "foo"}}'""",
-        shell=True)
-    assert '{"title": "foo"}' in result.decode('utf-8')
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['shapes', '{"tile": [106, 193, 9], "properties": {"title": "foo"}}'])
+    assert result.exit_code == 0
+    assert '{"title": "foo"}' in result.output
 
 
 def test_cli_tiles_no_bounds():
-    result = subprocess.check_output(
-        'echo "[-105, 39.99, -104.99, 40]" | mercantile tiles 14',
-        shell=True)
-    assert result.decode('utf-8').strip() == '[3413, 6202, 14]\n[3413, 6203, 14]'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['tiles', '14'], '[-105, 39.99, -104.99, 40]')
+    assert result.exit_code == 0
+    assert result.output == '[3413, 6202, 14]\n[3413, 6203, 14]\n'
+
 
 def test_cli_tiles_bounding_tiles():
-    result = subprocess.check_output(
-        'echo "[-105, 39.99, -104.99, 40]" | mercantile tiles --bounding-tile',
-        shell=True)
-    assert result.decode('utf-8').strip() == '[1706, 3101, 13]'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['tiles', '--bounding-tile'], '[-105, 39.99, -104.99, 40]')
+    assert result.exit_code == 0
+    assert result.output == '[1706, 3101, 13]\n'
+
 
 def test_cli_tiles_bounding_tiles_z0():
-    result = subprocess.check_output(
-        'echo "[-1, -1, 1, 1]" | mercantile tiles --bounding-tile',
-        shell=True)
-    assert result.decode('utf-8').strip() == '[0, 0, 0]'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['tiles', '--bounding-tile'], '[-1, -1, 1, 1]')
+    assert result.exit_code == 0
+    assert result.output == '[0, 0, 0]\n'
+
 
 def test_cli_tiles_bounds():
-    result = subprocess.check_output(
-        'echo "[-105, 39.99, -104.99, 40]" | mercantile tiles 14 - --with-bounds',
-        shell=True)
-    first, last = result.decode('utf-8').strip().split('\n')
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['tiles', '--with-bounds', '14'], '[-105, 39.99, -104.99, 40]')
+    assert result.exit_code == 0
+    first, last = result.output.strip().split('\n')
     assert [round(x, 3) for x in json.loads(first)][3:] == [-105.007, 39.994, -104.985, 40.011]
 
 
 def test_cli_tiles_implicit_stdin():
-    result = subprocess.check_output(
-        'echo "[-105, 39.99, -104.99, 40]" | mercantile tiles 14',
-        shell=True)
-    assert result.decode('utf-8').strip() == '[3413, 6202, 14]\n[3413, 6203, 14]'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['tiles', '14'], '[-105, 39.99, -104.99, 40]')
+    assert result.exit_code == 0
+    assert result.output == '[3413, 6202, 14]\n[3413, 6203, 14]\n'
 
 
 def test_cli_tiles_arg():
-    result = subprocess.check_output(
-        'mercantile tiles 14 "[-105, 39.99, -104.99, 40]"',
-        shell=True)
-    assert result.decode('utf-8').strip() == '[3413, 6202, 14]\n[3413, 6203, 14]'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['tiles', '14', '[-105, 39.99, -104.99, 40]'])
+    assert result.exit_code == 0
+    assert result.output == '[3413, 6202, 14]\n[3413, 6203, 14]\n'
+
 
 def test_cli_tiles_geosjon():
-    collection = {"features": [{"geometry": {"coordinates": [[[-105.46875, 39.909736], [-105.46875, 40.446947], [-104.765625, 40.446947], [-104.765625, 39.909736], [-105.46875, 39.909736]]], "type": "Polygon"}, "id": "(106, 193, 9)", "properties": {"title": "XYZ tile (106, 193, 9)"}, "type": "Feature"}], "type": "FeatureCollection"}
-    result = subprocess.check_output(
-        "echo '%s' | mercantile tiles 9" % json.dumps(collection),
-        shell=True)
-    result.decode('utf-8').strip() == '[]'
+    collection = '{"features": [{"geometry": {"coordinates": [[[-105.46875, 39.909736], [-105.46875, 40.446947], [-104.765625, 40.446947], [-104.765625, 39.909736], [-105.46875, 39.909736]]], "type": "Polygon"}, "id": "(106, 193, 9)", "properties": {"title": "XYZ tile (106, 193, 9)"}, "type": "Feature"}], "type": "FeatureCollection"}'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['tiles', '9'], collection)
+    assert result.exit_code == 0
+    assert result.output == '[106, 193, 9]\n[106, 194, 9]\n'
 
 
 def test_cli_parent():
-    result = subprocess.check_output(
-        'echo "[486, 332, 10]\n[486, 332, 10]" | mercantile parent',
-        shell=True)
-    assert result.decode('utf-8').strip() == '[243, 166, 9]\n[243, 166, 9]'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['parent'], '[486, 332, 10]\n[486, 332, 10]')
+    assert result.exit_code == 0
+    assert result.output == '[243, 166, 9]\n[243, 166, 9]\n'
 
-def test_cli_grandparent():
-    result = subprocess.check_output(
-        'echo "[486, 332, 10]" | mercantile parent | mercantile parent',
-        shell=True)
-    assert result.decode('utf-8').strip() == '[121, 83, 8]'
 
 def test_cli_parent_depth():
-    result = subprocess.check_output(
-        'echo "[486, 332, 10]" | mercantile parent --depth 2',
-        shell=True)
-    assert result.decode('utf-8').strip() == '[121, 83, 8]'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['parent', '--depth', '2'], '[486, 332, 10]')
+    assert result.exit_code == 0
+    assert result.output == '[121, 83, 8]\n'
 
 
 def test_cli_parent_multidepth():
-    result = subprocess.check_output(
-        'echo "[486, 332, 10]\n[121, 83, 8]" | mercantile parent --depth 2',
-        shell=True)
-    assert result.decode('utf-8').strip() == '[121, 83, 8]\n[30, 20, 6]'
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['parent', '--depth', '2'], '[486, 332, 10]\n[121, 83, 8]')
+    assert result.exit_code == 0
+    assert result.output == '[121, 83, 8]\n[30, 20, 6]\n'
 
 
 def test_cli_children():
-    result = subprocess.check_output(
-        'echo "[243, 166, 9]" | mercantile children',
-        shell=True)
-    assert result.decode('utf-8').strip() == """
-[486, 332, 10]
-[487, 332, 10]
-[487, 333, 10]
-[486, 333, 10]
-""".strip()
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ['children'], '[243, 166, 9]')
+    assert result.exit_code == 0
+    assert result.output == '[486, 332, 10]\n[487, 332, 10]\n[487, 333, 10]\n[486, 333, 10]\n'
 
 
 def test_cli_strict_overlap_contain():
-    result = subprocess.check_output(
-        'mercantile shapes "[2331,1185,12]" | mercantile tiles 12',
-        shell=True)
-    assert result.decode('utf-8').strip() == """
-[2331, 1185, 12]
-""".strip()
+    runner = CliRunner()
+    result1 = runner.invoke(
+        cli, ['shapes'], '[2331,1185,12]')
+    assert result1.exit_code == 0
+    result2 = runner.invoke(
+        cli, ['tiles', '12'], result1.output)
+    assert result2.exit_code == 0
+    assert result2.output == '[2331, 1185, 12]\n'
