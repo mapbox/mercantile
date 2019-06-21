2b8774c8960c89726eb6bfb2c110ccb7d9318c03
Sean Gillies <sean@mapbox.com>
2019-06-21 10:44:01 -0600

    Update version in setup script

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.rst", **open_kwds) as f:
 
 setup(
     name="mercantile",
-    version="1.0.4",
+    version="1.1.0",
     description="Web mercator XYZ tile utilities",
     long_description=readme,
     classifiers=[
