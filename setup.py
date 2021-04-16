d8cb2a7fb1b2bfa8d3b1a7072abcabc2247a788c
Sean Gillies <sean@mapbox.com>
2021-04-16 16:42:07 -0600

    Require hashes for dependencies

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=["click>=3.0"],
     extras_require={
         "dev": ["check-manifest"],
-        "test": ["coveralls", "hypothesis", "pytest-cov", "pydocstyle"],
+        "test": ["hypothesis", "pytest"],
     },
     entry_points="""
       [console_scripts]
